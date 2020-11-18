123fbb3adc176cb1ac92e47842ba1e1c6db34769
Yash Shrivastava <68012929+Yash4122000@users.noreply.github.com>
2020-11-18 18:12:06 +0530

    Integer Variables in python

--- Integer Variable.py
+++ Integer Variable.py
@@ -5,4 +5,4 @@ print(num1+num2)  #30
 print(num1*num2+num1/num2)  #200.5
 print("num1"+"num2")  #num1num2 :- here the variable in commas is considered as strings
 print(5*6)  #30
-print("5"+"6")  #56 
+print("5"+"6")  #56
