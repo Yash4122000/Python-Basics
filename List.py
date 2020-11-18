8d38aa381965489660854e128af500b328f64f24
Yash Shrivastava <68012929+Yash4122000@users.noreply.github.com>
2020-11-18 23:33:45 +0530

    List Basics

---
+++ List.py
@@ -0,0 +1,23 @@
+# LIST
+'''
+List is a variable in which different type of elements can be stored deperated with commas.
+List is mutable in nature wiz. the elements can be change once the list is formed.
+'''
+l=['a','b','c','d','1','2','3','4']
+print(l)    #['a', 'b', 'c', 'd', '1', '2', '3', '4']
+print(l[5])     #2
+print(l[-4])    #1
+print(l[:])     #['a', 'b', 'c', 'd', '1', '2', '3', '4']
+print(l+['X','Y','Z'])  #['a', 'b', 'c', 'd', '1', '2', '3', '4', 'X', 'Y', 'Z']
+l[3:5]=['#','%']        
+print(l)        #['a', 'b', 'c', '#', '%', '2', '3', '4']
+l[3:4]=[]
+print(l)        #['a', 'b', 'c', '%', '2', '3', '4']
+
+#Length of List
+
+print(len(l))   #7
+
+m=['A','B','C','D','!','@','#','$']
+n=[l,m]
+print(n)        #[['a', 'b', 'c', '%', '2', '3', '4'], ['A', 'B', 'C', 'D', '!', '@', '#', '$']]
