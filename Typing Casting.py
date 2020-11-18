8f10c522ca2fed6e0eea3bd0efb1d7e42c53e9ba
Yash Shrivastava <68012929+Yash4122000@users.noreply.github.com>
2020-11-18 19:42:42 +0530

    Typing Casting Variables

---
+++ Typing Casting.py
@@ -0,0 +1,23 @@
+#Typing Casting
+i=104
+s="104"
+print(i)    #104
+print(s)    #104
+
+'''
+print(i+s) ||  This will give error as if 'i' is integer and 's' is string 
+
+'''
+
+#To do the previous print possible we need to do TYPE CASTING
+s=int(104)
+print(i+s)  #here the variable is converyed to integer  :- 208
+
+#Type Casting to string
+i=str(104)
+s="104"
+print(i+s)  #adding strings append them :- 104104
+
+#Type Casting to float
+i=float(104)
+print(i)    #104.0
